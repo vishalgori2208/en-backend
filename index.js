577841cb1c3dd67df21af612391c7b67e1ef6f46
vishalgori2208 <137760873+vishalgori2208@users.noreply.github.com>
2023-06-26 14:30:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,39 +8,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function sendEmail(name, phone, query){
+function sendEmail(name, phone, query) {
     let transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASSWORD
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
         }
     });
     let mailOptions = {
-        from:process.env.EMAIL_USER,
-        to:"vishalgori2208@gmail.com",
+        from: process.env.EMAIL_USER,
+        to: "vishalgori2208@gmail.com",
         subject: "Enquiry form " + name,
         text: phone + " " + query
     }
 
     transporter.sendMail(mailOptions, (err, info) => {
         if (err)
-        console.log("Mail err " + err);
+            console.log("Mail err " + err);
         else
-        console.log("mail send", info.response);
+            console.log("mail send", info.response);
     })
 }
 
-app.post("/save", (req,res) => {
+app.post("/save", async (req, res) => {
     const name = req.body.name;
     const phone = req.body.phone;
     const query = req.body.query;
     console.log(name + " " + phone + " " + query);
-    sendEmail(name,phone,query);
-    res.send("success");
+    try {
+        await sendEmail(name, phone, query);
+        res.send("success");
+    }
+    catch(err){
+        res.send("failed");
+    }
+    
 })
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port,()=>{
+const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
