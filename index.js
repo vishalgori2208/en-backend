b3b846a18083462ff5f995c0d20dc1c2fcace29a
vishalgori2208 <137760873+vishalgori2208@users.noreply.github.com>
2023-06-26 13:21:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,46 +7,60 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function sendEmail(name, phone, query){
+function sendEmail(name, phone, query) {
     let transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASSWORD
+        service: "gmail",
+        auth: {
+            user: "2021.vishal.gori@ves.ac.in",
+            pass: "fdhtpejbnptgfswp"
         }
     });
     let mailOptions = {
-        from:"2021.vishal.gori@ves.ac.in",
-        to:"vishalgori2208@gmail.com",
+        from: "2021.vishal.gori@ves.ac.in",
+        to: "vishalgori2208@gmail.com",
         subject: "Enquiry form " + name,
         text: phone + " " + query
     }
 
     transporter.sendMail(mailOptions, (err, info) => {
         if (err)
-        console.log("Mail err " + err);
+            console.log("Mail err " + err);
         else
-        console.log("mail send", info.response);
+            console.log("mail send", info.response);
     })
 }
 
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        status:'Success',
-        message:'Your api is working fine'
-    })
-})
-
-app.post("/save", async(req,res) => {
+app.post("/save", (req, res) => {
     const name = req.body.name;
     const phone = req.body.phone;
     const query = req.body.query;
     console.log(name + " " + phone + " " + query);
-    await sendEmail(name,phone,query);
-    res.send("success");
-})
+    try {
+        sendEmail(name, phone, query);
+        res.send("success");
+        res.status(200).json({
+            status: 'success',
+            message: 'Mail sent successfully'
+        })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
+
+});
+
+app.all('*',(req,res)=>{
+    res.status(400).json({
+        status:'fail',
+        message:'An unexpected error has occured'
+    })
+});
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port,()=>{
+const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
